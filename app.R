ae37454f411fbb7e278eaf4b54afd46e0ab5d059
Melissa Weidlich-Rau <113716944+xxelementsxx@users.noreply.github.com>
2023-02-02 01:17:42 +0100

    - see first commit - + Ladefehler Ideen

--- app.R
+++ app.R
@@ -7,18 +7,20 @@ pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 BirdNETmonitor::run_app() # add parameters here (if any)
 
 ##XXExport all backEnd-Data into an excel-file
-if (!require(writexl)) install.packages("writexl")
-if (!require (readxl)) install.packages("readxl")
-if (!require(tidyverse)) install.packages("tidyverse")
+#if (!require(writexl)) install.packages("writexl")
+#if (!require (readxl)) install.packages("readxl")
+#if (!require(tidyverse)) install.packages("tidyverse")
 
-library(writexl)
-library(readxl)
-library(tidyverse)
+#library(writexl)
+#library(readxl)
+#library(tidyr)
+#library(tidyverse)
+#library(RCurl)
 
 ##XXWrite Excel-File
-dir.create("7777")
-source("http://rfunction.com/code/1203/120312.R") ##should be "data"
-list.files("7777")
+#dir.create("7777")
+#source("http://rfunction.com/code/1203/120312.R") ##should be "data"
+#list.files("7777")
 
 
-write::write_xlsx(data, path = "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/logs/dataBEXX.xlsx")
+#write::write_xlsx(data, path = "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/logs/dataBEXX.xlsx")
