d8ed212b38e82240518a4ad171232242db7b6f22
Melissa Weidlich-Rau <113716944+xxelementsxx@users.noreply.github.com>
2023-02-03 10:33:22 +0100

    - Custom Branding

--- R/run_app.R
+++ R/run_app.R
@@ -8,21 +8,39 @@
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
 run_app <- function(
-  onStart = NULL,
-  options = list(),
-  enableBookmarking = NULL,
-  uiPattern = "/",
-  ...
-) {
+    onStart = NULL,
+    options = list(),
+    enableBookmarking = NULL,
+    uiPattern = "/",
+    ...
+){
   polished::polished_config(
     app_name = "BirdNETmonitor",
     api_key = "1AjI1zHTrL6k2C4qoTQg2aoN9DqcJRmTdc",
     cookie_expires = NULL
   )
+  my_custom_sign_in_page <- sign_in_ui_default(
+    color = "#006CB5",
+    company_name = "Tychobra",
+    logo_top = tags$img(
+      src = "www/logo-birdnet_icon.png",
+      alt = "BirdNET Logo",
+      style = "width: 125px; margin-top: 30px; margin-bottom: 30px;"
+    ),
+    logo_bottom = tags$img(
+      src = "www/logo-birdnet_icon.png",
+      alt = "Cornell Logo",
+      style = "width: 200px; margin-bottom: 15px; padding-top: 15px;"
+    ),
+    icon_href = "www/logo-birdnet_icon.png",
+    background_image = "www/logo-birdnet_icon.png"
+  )
+
+
 
   with_golem_options(
     app = shinyApp(
-      ui = polished::secure_ui(app_ui), #app_ui,
+      ui = polished::secure_ui(app_ui,sign_in_page_ui = my_custom_sign_in_page), #app_ui,
       server = polished::secure_server(app_server), #app_server,
       onStart = onStart,
       options = options,
@@ -31,4 +49,7 @@ run_app <- function(
     ),
     golem_opts = list(...)
   )
+
 }
+
+
