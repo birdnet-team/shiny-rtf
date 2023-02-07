9f472e45089d2c99d6e16df056da7200a88786c7
Felix Günther <felix.gnthr@gmail.com>
2023-02-07 14:38:52 +0100

    adds birdnames

--- data-raw/birdnames.R
+++ data-raw/birdnames.R
@@ -1,7 +1,7 @@
 ## code to prepare `birdnames` dataset goes here
 
 birdnames <-
-  readr::read_delim("data-raw/BirdNET_HI_2022_Labels_and_codes.txt", delim = ",", col_names = c("names", "codes")) %>%
+  readr::read_delim("data-raw/BirdNET_HI_2022_Labels_and_codes.txt", delim = ",", col_names = c("names", "code")) |>
   tidyr::separate_wider_delim(cols = "names", delim = "_", names = c("scientifc", "common"))
 
 
