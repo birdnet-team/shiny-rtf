3f5a5fb101fb6a1ad04c3fe8015e529da5d4d3a2
Felix Günther <felix.gnthr@gmail.com>
2023-02-10 23:29:57 +0100

    remove old debugging code

--- R/app_server.R
+++ R/app_server.R
@@ -16,22 +16,9 @@ app_server <- function(input, output, session) {
   # Header ----------------------------------------------------------------------------------------------------------
   tz <- mod_set_timezone_server("set_timezone_1")
 
-  observe({
-    req(tz())
-    golem::message_dev("TIMEZONE SERVER")
-    golem::print_dev(tz())
-  })
-
   # Get Detections and logs
   data <- mod_get_data_daterange_server("get_data_daterange_1", url, tz_server = "HST", tz_out = tz)
 
-  #   observe({
-  #   golem::message_dev("DATA")
-  #   golem::print_dev(dplyr::glimpse(data$detections))
-  #   golem::print_dev(dplyr::glimpse(data$logs))
-  #   golem::print_dev(data$detections$datetime[1:2])
-  #   golem::print_dev(data$logs$datetime_pi[1:2])
-  # })
 
   # Overview --------------------------------------------------------------------------------------------------------
   mod_status_overview_server("status_overview_1", data)
