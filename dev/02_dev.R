caea1a0b7f7ca303bbc99ef72ddefa9cab0e3894
Felix Günther <felix.gnthr@gmail.com>
2023-03-21 15:00:53 +0100

    removes deprec. function

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -33,8 +33,6 @@ golem::add_module(name = "global_filter", with_test = FALSE)
 ## Add helper functions ----
 ## Creates fct_* and utils_*
 golem::add_fct("dataListFilter", with_test = FALSE)
-golem::add_fct("get_browser_timezone", with_test = FALSE)
-
 golem::add_utils("helpers", with_test = TRUE)
 
 ## External resources
