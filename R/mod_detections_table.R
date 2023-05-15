daa491c0cf1638c9002a9274774b0b631284abd2
Melissa Weidlich-Rau <113716944+xxelementsxx@users.noreply.github.com>
2023-05-15 15:16:31 +0200

    - includes download and play option (#18)
    * - includes download and play option
    
    * only play audio when clickimng on play button and not anywhere in the row
    
    * removes download button since it does not work
    
    ---------
    
    Co-authored-by: Felix Günther <felix.gnthr@gmail.com>

--- R/mod_detections_table.R
+++ R/mod_detections_table.R
@@ -29,7 +29,9 @@ mod_detections_table_server <- function(id, data) {
       req(data$detections)
       data$detections %>%
         mutate(
-          datetime = strftime(datetime, "%F %T", tz = lubridate::tz(datetime))
+          datetime = strftime(datetime, "%F %T", tz = lubridate::tz(datetime)),
+          sound_play = paste0('https://reco.birdnet.tucmi.de/reco/det/', uid, '/audio')
+
         ) %>%
         dplyr::relocate(common, .after = recorder_id)
     })
@@ -60,16 +62,17 @@ mod_detections_table_server <- function(id, data) {
             name = "Species",
             filterInput = dataListFilter("detections-list")
           ),
+          snippet_path = colDef(show = FALSE),
           scientific = colDef(show = FALSE),
           species_code = colDef(show = FALSE),
-          snippet_path = colDef(
-            name = "audio",
+          sound_play = colDef(
+            name = "Audio",
             html = TRUE,
             cell = function(value) {
               if (value == "None") {
                 '<i class="fa-solid fa-music", style = "color:#eaecee "></i>'
               } else {
-                '<i class="fa-solid fa-music", style = "color:#008080"></i>'
+                paste0('<audio controls><source src="', value, '" type="audio/wav"></audio>')
               }
             }
           ),
@@ -79,30 +82,12 @@ mod_detections_table_server <- function(id, data) {
           confidence = colDef(
             filterable = TRUE,
             filterMethod = JS("function(rows, columnId, filterValue) {
-                return rows.filter(function(row) {
-                  return row.values[columnId] >= filterValue
-                })
-              }"),
-          )
-        ),
-        onClick = JS("function(rowInfo, column) {
-            // Only handle click events on the 'details' column
-            //if (column.id !== 'audio') {
-            //  return
-            //}
-
-            // Display an alert dialog with details for the row
-            //window.alert('Details for row ' + rowInfo.index + ':\\n' + rowInfo.values.audio_url)
-            var audio_url = 'https://reco.birdnet.tucmi.de/reco/det/' + rowInfo.values.uid + '/audio'
-            var audio = new Audio(audio_url);
-            audio.play();
-
-            // Send the click event to Shiny, which will be available in input$show_details
-            // Note that the row index starts at 0 in JavaScript, so we add 1
-            //if (window.Shiny) {
-            //  Shiny.setInputValue('show_details', { index: rowInfo.index + 1 }, { priority: 'event' })
-            //}
+            return rows.filter(function(row) {
+              return row.values[columnId] >= filterValue
+            })
           }")
+          )
+        )
       )
     })
   })
