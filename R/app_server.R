96c1091088f45491634b0b62dc51ecc1f976ffab
Felix Günther <felix.gnthr@gmail.com>
2023-06-15 16:23:51 +0200

    include pkgload

--- R/app_server.R
+++ R/app_server.R
@@ -2,7 +2,7 @@
 #'
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
-#' @import shiny
+#' @import shiny pkgload
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
