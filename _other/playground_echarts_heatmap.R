81d6f008743166496bdca5115d06394346d450c6
Felix Günther <felix.gnthr@gmail.com>
2023-09-04 12:06:16 +0200

    WIP

--- _other/playground_echarts_heatmap.R
+++ _other/playground_echarts_heatmap.R
@@ -19,21 +19,30 @@ recorders <- get_recorders(url = url)
 # Detections ------------------------------------------------------------------------------------------------------
 
 # species <- c("hawama")
-species <- "afrsil1"
+species <- "houfin"
 # params <- list("datetime__gte" = "2023-07-28", "species_code" = species, "recorder_id" = recorders$recorder_id[1])
 # get_detections(url, params = params)
 
 paramas <- list("datetime__gte" = "2023-01-01", "species_code" = species, "confidence__gte" = 0.1 )
-dets <- lapply(recorders$recorder_id[c(1,4)], function(x) {
+dets <- lapply(recorders$recorder_id, function(x) {
   #print(c(params, "recorder_id" = x))
   get_detections(url, params = c(paramas, "recorder_id" = x))
 }) |> do.call(what = rbind, args = _)
-
-
+#
+#dets <- get_detections(url, params = c("datetime__gte" = "2023-08-31", "recorder_id" = "BirdNET-HI001"))
+#
+# length(dets) == length(expected_detections_response)
+#
+# all(names(dets) == names(expected_detections_response))
+#
+# classes_dets <- sapply(dets, class)
+# classes_expected <- sapply(expected_detections_response, class)
+#
+# all(classes_dets == classes_expected)
 
 calendar_dats <-
   dets |>
-  filter(confidence > 0.9) |>
+  #filter(confidence > 0.8) |>
   mutate(
     datetime = lubridate::force_tz(datetime, "US/Hawaii"),
     datetime_call = datetime + lubridate::seconds(start),
@@ -42,8 +51,8 @@ calendar_dats <-
   count(agg_timeunit) |>
   # fill in "time slots" that are missing in the sequence by recorder
   tidyr::complete(agg_timeunit = seq(
-    ymd_hms("2023-01-01 00:00:00"),
-    ymd_hms("2023-12-31 23:59:59"),
+    lubridate::floor_date(min(agg_timeunit), unit = "day") - days(3), #ymd_hms("2023-01-01 00:00:00"),
+    lubridate::ceiling_date(max(agg_timeunit), unit = "day") + days(3), # ymd_hms("2023-12-31 23:59:59"),
     by = "10 min"
   ),
   fill = list(n = 0L)) |>
@@ -80,67 +89,64 @@ suntimes <-
   )
 
 
-heatmap_suntimes <-
-  calendar_dats |>
-  left_join(suntimes) |>
-  #select(date, time, time_seconds, n, time_sunrise_rounded, time_sunset_rounded) |>
-  distinct()
+# heatmap_suntimes <-
+#   calendar_dats |>
+#   left_join(suntimes) |>
+#   #select(date, time, time_seconds, n, time_sunrise_rounded, time_sunset_rounded) |>
+#   distinct()
 #
 # heatmap_suntimes |>
 #   filter(date > "2023-10-30") |> View()
-
 calendar_dats |>
   mutate(n = if_else(n == 0, NA, n)) |>
   e_charts(date) |>
-  e_heatmap(time, n) |>
+  e_heatmap(time, n, emphasis = list(disabled = TRUE)) |>
   e_visual_map(
     n,
     type = "continuous",
     calculable = FALSE,
-    color = rev(c("#fcde9c","#faa476","#f0746e","#e34f6f","#dc3977","#b9257a","#7c1d6f")),
-    right = "0%",
-    bottom = "15%"
+    color = rev(c("#fcde9c", "#faa476", "#f0746e", "#e34f6f", "#dc3977", "#b9257a", "#7c1d6f")),
+    right = "1%",
+    bottom = "2%"
   ) |>
   e_tooltip(formatter = htmlwidgets::JS("
               function(params){
                 return('<strong>' + params.name + '</strong>' +
-                        '<br />detections: ' + params.value[2] +
-                        '<br />' + 'Date: ' + params.value[0] +
-                        '<br />' + 'Time: ' + params.value[1]
+                        '<br />' + 'Time: ' + params.value[1] +
+                        '<br />Detections: ' + params.value[2]
                 )
               }
-    ")) |>
+        ")) |>
   e_data(suntimes, date) |>
   e_line(time_sunset_seconds, symbol = "none", legend = FALSE, y_index = 1, x_index = 1) |>
   e_line(time_sunrise_seconds, symbol = "none", legend = FALSE, y_index = 1, x_index = 1) |>
-  e_y_axis(type = "category", index = 0) |>
-  e_y_axis(type = "value", index = 1, min = 0, max = 24*60*60, show = FALSE) |>
-  e_x_axis(offset = 5, type = "category", margin = 5, index = 0) |>
-  e_x_axis(offset = 5, type = "time", margin = 5, index = 1, show = FALSE) |>
-  #e_datazoom(type = "slider", xAxisIndex = 1, start = 100, end = 0, brushSelect = FALSE, height = 20) %>%
+  e_y_axis(index = 0, type = "category") |>
+  e_y_axis(index = 1, type = "value", min = 0, max = 24 * 60 * 60, show = FALSE) |>
+  e_x_axis(index = 0, offset = 5, type = "category", margin = 5) |>
+  e_x_axis(index = 1, offset = 5, type = "time", margin = 5, show = FALSE) |>
+  e_grid(
+    containLabel = TRUE,
+    left = "left",
+    top = "middel",
+    bottom = "2%",
+    right = "5%"
+  ) |>
+  # e_datazoom(type = "slider", xAxisIndex = 1, start = 100, end = 0, brushSelect = FALSE, height = 20) %>%
   identity()
-
 
 
 calendar_dats |>
   mutate(n = if_else(n == 0, NA, n)) |>
   e_charts(date) |>
-  e_heatmap(time, n, name = "detections", itemStyle = list(borderWidth = 2)) |>
-  e_data(suntimes, date) |>
-  e_line(time_sunset_seconds, symbol = "none", lineStyle = list(color = "#5c53a5"), legend = FALSE, y_index = 1, x_index = 1, name = "sunset") |>
-  e_line(time_sunrise_seconds, symbol = "none", lineStyle = list(color = "#5c53a5"), legend = FALSE, y_index = 1, x_index = 1, name = "sunrise") |>
-  e_x_axis(index = 0, offset = 5, type = "category", margin = 5) |>
-  e_y_axis(index = 0, type = "category") |>
-  e_y_axis(index = 1, type = "value", min = 0, max = 24*60*60, show = FALSE) |>
-  e_x_axis(index = 1, offset = 5, type = "time", margin = 5, show = FALSE) |>
-  #e_datazoom(type = "slider", xAxisIndex = 1, start = 100, end = 0, brushSelect = FALSE, height = 20) %>%
+  e_heatmap(time, n, emphasis = list(disabled = TRUE)) |>
   e_visual_map(
-    serie = n,
+    n,
     type = "continuous",
+    min = 0,
+    max = 10,
     calculable = FALSE,
     color = rev(c("#fcde9c","#faa476","#f0746e","#e34f6f","#dc3977","#b9257a","#7c1d6f")),
-    right = "0%",
-    bottom = "15%"
+    position = "insideBottomRight"
   ) |>
   e_tooltip(formatter = htmlwidgets::JS("
               function(params){
@@ -151,16 +157,24 @@ calendar_dats |>
                 )
               }
     ")) |>
+  e_data(suntimes, date) |>
+  e_line(time_sunset_seconds, symbol = "none", legend = FALSE, y_index = 1, x_index = 0) |>
+  e_line(time_sunrise_seconds, symbol = "none", legend = FALSE, y_index = 1, x_index = 0) |>
+  e_y_axis(index = 0, type = "category") |>
+  e_y_axis(index = 1, type = "value", min = 0, max = 24 * 60 * 60, show = TRUE) |>
+  e_x_axis(index = 0, offset = 5, type = "category", margin = 5) |>
+  e_datazoom(type = "slider", xAxisIndex = 0, start = 100, end = 0, brushSelect = TRUE, height = 20) %>%
+  #e_datazoom(type = "inside") |>
   identity()
 
 
 
 
-e_formatter_seconds_to_hhmmss <- function() {
-  htmlwidgets::JS(
-      "function(value, index) {
-        var timestamp = new Date(value * 1000).toISOString().substring(11, 16);
-        return timestamp;
-    }"
-  )
-}
+# e_formatter_seconds_to_hhmmss <- function() {
+#   htmlwidgets::JS(
+#       "function(value, index) {
+#         var timestamp = new Date(value * 1000).toISOString().substring(11, 16);
+#         return timestamp;
+#     }"
+#   )
+# }
