b7357b3cf46605f00aa20b6d7356b681ea72cf4a
Felix Günther <felix.gnthr@gmail.com>
2023-09-20 10:01:45 +0200

    adds non bird categrie objects

--- data-raw/birdnames.R
+++ data-raw/birdnames.R
@@ -5,3 +5,30 @@ birdnames <-
   tidyr::separate_wider_delim(cols = "names", delim = "_", names = c("scientific", "common"))
 
 usethis::use_data(birdnames, overwrite = TRUE)
+
+other_categories <-
+  tibble::tribble(
+    ~common, ~code,
+    "Dog",     "dogdog",
+    "Engine",     "engine",
+    "Environmental",     "envrnm",
+    "Fireworks",     "frwrks",
+    "Gun",     "gungun",
+    "Human non-vocal",     "humnov",
+    "Human vocal",     "humvoc",
+    "Human whistle",     "humwhi",
+    "Noise",     "nocall",
+    "Power tools",     "powtoo",
+    "Siren",     "siren1"
+  )
+
+usethis::use_data(other_categories, overwrite = TRUE)
+
+
+all_categories <-
+  other_categories |>
+  dplyr::mutate(scientific = common) |>
+  dplyr::bind_rows(birdnames)
+
+
+usethis::use_data(all_categories, overwrite = TRUE)
