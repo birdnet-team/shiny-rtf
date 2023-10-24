22e9b0f5826c60d8c497b68e0432eeabc70aa1e5
Melissa Weidlich-Rau <113716944+xxelementsxx@users.noreply.github.com>
2023-10-24 11:34:32 +0200

    xx serverconnection

--- R/RealtimeDisplay.R
+++ R/RealtimeDisplay.R
@@ -2,75 +2,82 @@
 #' @import httr
 #' @import png
 
-Callcam <- fluidPage(
-  titlePanel("Real-Time Display of Observation Images from Units"),
+Callcam <- function(id){
+ # ns <- NS(id)
+  titlePanel("Real-Time Display of Observation Images from Units")
 
-  mainPanel(
-    actionButton("displayBtnUnit1", "show Picture Unit 1"),
-    actionButton("displayBtnUnit2", "show Picture Unit 2"),
-    actionButton("displayBtnUnit3", "show Picture Unit 3"),
-    uiOutput("imageOutputUnit1"),
-    uiOutput("imageOutputUnit2"),
-    uiOutput("imageOutputUnit3")
-  )
-)
+ # mainPanel(
+    # actionButton(ns("displayBtnUnit1", label = "show Picture Unit 1")),
+    # actionButton(ns("displayBtnUnit2", label = "show Picture Unit 2")),
+    # actionButton(ns("displayBtnUnit3", label = "show Picture Unit 3")),
+    # actionButton("displayBtnUnit1", "show Picture Unit 1"),
+    # actionButton("displayBtnUnit2", "show Picture Unit 2"),
+    # actionButton("displayBtnUnit3", "show Picture Unit 3"),
+    # uiOutput("imageOutputUnit1"),
+    # uiOutput("imageOutputUnit2"),
+    # uiOutput("imageOutputUnit3")
+ # )
 
-callcam_server <- function(input, output) {
-  # img_path1 <- "img1.png"
-  # img_path2 <- "img2.png"
-  # img_path3 <- "img3.png"
-  #
-  # urlUnit1 <- "http://viewer:birdnet2023!@166.148.48.130:8001/cgi-bin/image.jpg?imgprof=BirdNET"
-  # urlUnit2 <- "http://viewer:birdnet2023!@166.143.21.131:8001/cgi-bin/image.jpg?imgprof=BirdNET"
-  # urlUnit3 <- "http://viewer:birdnet2023!@166.148.204.108:8001/cgi-bin/image.jpg?imgprof=BirdNET"
-  #
-  # observeEvent(input$displayBtnUnit1, {
-  #   download.file(urlUnit1, "img1.png", mode = "wb")
-  #   output$imageOutputUnit1 <- renderUI({
-  #     fluidRow(
-  #       column(5, imageOutput("imgOutput1"))
-  #     )
-  #   })
-  # })
-  #
-  # observeEvent(input$displayBtnUnit2, {
-  #   download.file(urlUnit2, "img2.png", mode = "wb")
-  #   output$imageOutputUnit2 <- renderUI({
-  #     fluidRow(
-  #       column(5, imageOutput("imgOutput2")
-  #       )
-  #     )
-  #   })
-  # })
-  #
-  # observeEvent(input$displayBtnUnit3, {
-  #   download.file(urlUnit3, "img3.png", mode = "wb")
-  #   output$imageOutputUnit3 <- renderUI({
-  #     fluidRow(
-  #       column(5, imageOutput("imgOutput3"),
-  #       )
-  #     )
-  #   })
-  # })
-  #
-  #
-  # output$imgOutput1 <- renderImage({
-  #   list(src = img_path1,
-  #        style = "max-width:100%",
-  #        alt = "Unit 1 nicht gefunden")
-  # }, deleteFile = FALSE)
-  #
-  # output$imgOutput2 <- renderImage({
-  #   list(src = img_path2,
-  #        style = "max-width:100%",
-  #        alt = "Unit 2 nicht gefunden")
-  # }, deleteFile = FALSE)
-  #
-  # output$imgOutput3 <- renderImage({
-  #   list(src = img_path3,
-  #        style = "max-width:100%",
-  #        alt = "Unit 3 nicht gefunden")
-  # }, deleteFile = FALSE)
 }
 
+callcam_server <- function(id, data) {
+  shinyServer(function(input, output, session) {
+
+  #   img_path1 <- "img1.png"
+  #   img_path2 <- "img2.png"
+  #   img_path3 <- "img3.png"
+  #
+  #   urlUnit1 <- "http://viewer:birdnet2023!@166.148.48.130:8001/cgi-bin/image.jpg?imgprof=BirdNET"
+  #   urlUnit2 <- "http://viewer:birdnet2023!@166.143.21.131:8001/cgi-bin/image.jpg?imgprof=BirdNET"
+  #   urlUnit3 <- "http://viewer:birdnet2023!@166.148.204.108:8001/cgi-bin/image.jpg?imgprof=BirdNET"
+  #
+  #   observeEvent(input$displayBtnUnit1, {
+  #     download.file(urlUnit1, "img1.png", mode = "wb")
+  #     output$imageOutputUnit1 <- renderUI({
+  #       fluidRow(
+  #         column(5, imageOutput("imgOutput1"))
+  #       )
+  #     })
+  #   })
+  #
+  #   observeEvent(input$displayBtnUnit2, {
+  #     download.file(urlUnit2, "img2.png", mode = "wb")
+  #     output$imageOutputUnit2 <- renderUI({
+  #       fluidRow(
+  #         column(5, imageOutput("imgOutput2"))
+  #       )
+  #     })
+  #   })
+  #
+  #   observeEvent(input$displayBtnUnit3, {
+  #     download.file(urlUnit3, "img3.png", mode = "wb")
+  #     output$imageOutputUnit3 <- renderUI({
+  #       fluidRow(
+  #         column(5, imageOutput("imgOutput3"))
+  #       )
+  #     })
+  #   })
+  #
+  #   output$imgOutput1 <- renderImage({
+  #     list(src = img_path1,
+  #          style = "max-width:100%",
+  #          alt = "Unit 1 nicht gefunden")
+  #   }, deleteFile = FALSE)
+  #
+  #   output$imgOutput2 <- renderImage({
+  #     list(src = img_path2,
+  #          style = "max-width:100%",
+  #          alt = "Unit 2 nicht gefunden")
+  #   }, deleteFile = FALSE)
+  #
+  #   output$imgOutput3 <- renderImage({
+  #     list(src = img_path3,
+  #          style = "max-width:100%",
+  #          alt = "Unit 3 nicht gefunden")
+  #   }, deleteFile = FALSE)
+  #
+   })
+}
+
+#shiny::runApp('R/RealtimeDisplay.R')
 #shinyApp(Callcam, callcam_server)
