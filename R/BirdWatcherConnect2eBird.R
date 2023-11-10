3ecf7afa50bb540c242628fdc941a018054a1bfe
Melissa Weidlich-Rau <113716944+xxelementsxx@users.noreply.github.com>
2023-11-10 11:31:50 +0100

    script function set up mwr xx999

--- R/BirdWatcherConnect2eBird.R
+++ R/BirdWatcherConnect2eBird.R
@@ -22,7 +22,7 @@ wiki_server <- function(id, data) {
 
   image_data <- data.frame(
     image_path = c(
-      "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/www/Amandava_amandava_Red_Avadavat.png",
+      "inst/app/www/1_Amandava_amandava_Red_Avadavat.png",
       "www/second_image.png",
       "www/third_image.png",
       "www/fourth_image.png",
