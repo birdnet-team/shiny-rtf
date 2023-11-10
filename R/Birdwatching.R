09335fb3e8ab930824207a596a366c44ab715971
Melissa Weidlich-Rau <113716944+xxelementsxx@users.noreply.github.com>
2023-11-10 10:47:57 +0100

    BirdWatcher script done mwr xx999

--- R/Birdwatching.R
+++ R/Birdwatching.R
@@ -13,16 +13,17 @@ Wiki <- function(id) {
 
 wiki_server <- function(input, output, session) {
   observeEvent(input$image_click1, {
-    shinyjs::runjs("window.open('https://de.wikipedia.org/wiki/Vogel', '_blank');")
+    shinyjs::runjs("window.open('https://ebird.org/species/redava', '_blank');")#Tüpfelastrild
   })
 
-  # Verwenden Sie Schrägstriche in Pfadangaben und korrigieren Sie die paste0-Funktion
-  image_path1 <- reactiveVal("www/Amandava_amandava_Red_Avadavat.png")
+  image_path1 <- reactiveVal("C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/www/Amandava_amandava_Red_Avadavat.png")
 
   output$imageDisplay1 <- renderImage({
     list(src = image_path1(), height = 300)
   }, deleteFile = FALSE)
 }
 
+
+
 # Starten Sie die Shiny-App
-shinyApp(ui = Wiki(), server = wiki_server)
+#shinyApp(ui = Wiki(), server = wiki_server)
