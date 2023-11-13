8cadda1b0b64eb611d2cf9df36c50999a7f75b12
Melissa Weidlich-Rau <113716944+xxelementsxx@users.noreply.github.com>
2023-11-13 09:43:55 +0100

    change name mwr xx999

--- R/app_ui.R
+++ R/app_ui.R
@@ -37,7 +37,7 @@ app_ui <- function(request) {
           menuItem("Phenology", tabName = "phenology", icon = icon("chart-column")),
           menuItem("Detections", tabName = "detections", icon = icon("music")),
           menuItem("Observations", tabName = "observations", icon = icon("camera")),
-          menuItem("Encyclopedia", tabName = "watcher", icon = icon("search")),
+          menuItem("Encyclopedia", tabName = "encyclopedia", icon = icon("search")),
           menuItem("Health", tabName = "health", icon = icon("wave-square"))
         )
       ),
@@ -64,7 +64,7 @@ app_ui <- function(request) {
           ),
 
           tabItem(
-            tabName = "watcher",
+            tabName = "encyclopedia",
             Wiki("birdwiki")
           ),
 
