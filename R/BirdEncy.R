42a3e21741db7e9ed64e9cd6abe4d8d85963a9d4
Melissa Weidlich-Rau <113716944+xxelementsxx@users.noreply.github.com>
2023-11-14 15:14:19 +0100

    xx

--- R/BirdEncy.R
+++ R/BirdEncy.R
@@ -7,9 +7,17 @@ Wiki <- function(id) {
   fluidPage(
     titlePanel("Hawaiian Birdwatching Encyclopedia"),
     fluidRow(
-      lapply(1:11, function(i) {
-        column(2, imageOutput(ns(paste0("imageDisplay", i))))
-      })
+      column(2, imageOutput1(ns("imageDisplay1"))),
+      column(2, imageOutput2(ns("imageDisplay2"))),
+      column(2, imageOutput3(ns("imageDisplay3"))),
+      column(2, imageOutput4(ns("imageDisplay4"))),
+      column(2, imageOutput5(ns("imageDisplay5"))),
+      column(2, imageOutput6(ns("imageDisplay6"))),
+      column(2, imageOutput7(ns("imageDisplay7"))),
+      column(2, imageOutput8(ns("imageDisplay8"))),
+      column(2, imageOutput9(ns("imageDisplay9"))),
+      column(2, imageOutput10(ns("imageDisplay10"))),
+      column(2, imageOutput11(ns("imageDisplay11")))
     ),
     useShinyjs()
   )
@@ -19,49 +27,28 @@ wiki_server <- function(id, data) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    image_paths <- c(
-      "BirdNETmonitor/BirdWatcherImagesXX/1_Amandava_amandava_Red_Avadavat.png",
-      "BirdNETmonitor/BirdWatcherImagesXX/2_Northern_Pintails_(Male_&_Female)_I_IMG_0911.png",
-      "BirdNETmonitor/BirdWatcherImagesXX/3_Green-winged_Teal,_Port_Aransas,_Texas.png",
-      "BirdNETmonitor/BirdWatcherImagesXX/Anas platyrhynchos_Mallard.png",
-      "BirdNETmonitor/BirdWatcherImagesXX/Anous minutus_Black Noddy.png",
-      "BirdNETmonitor/BirdWatcherImagesXX/Anous stolidus_Brown Noddy.png",
-      "BirdNETmonitor/BirdWatcherImagesXX/Anser albifrons_Greater White-fronted Goose.png",
-      "BirdNETmonitor/BirdWatcherImagesXX/Anser caerulescens_Snow Goose.png",
-      "BirdNETmonitor/BirdWatcherImagesXX/Ardea herodias_Great Blue Heron.png",
-      "BirdNETmonitor/BirdWatcherImagesXX/Ardenna grisea_Sooty Shearwater.png",
-      "BirdNETmonitor/BirdWatcherImagesXX/Ardenna pacifica_Wedge-tailed Shearwater.png"
-    )
+    image_path1 <- reactiveVal("BirdNETmonitor/BirdWatcherImagesXX/1_Amandava_amandava_Red_Avadavat.png")
+    image_path2 <- reactiveVal("BirdNETmonitor/BirdWatcherImagesXX/2_Northern_Pintails_(Male_&_Female)_I_IMG_0911.png")
+    image_path3 <- reactiveVal("BirdNETmonitor/BirdWatcherImages/3_Green-winged_Teal,_Port_Aransas,_Texas.png")
+    image_path4 <- reactiveVal("BirdNETmonitor/BirdWatcherImages/Anas platyrhynchos_Mallard.png")
+    image_path5 <- reactiveVal("BirdNETmonitor/BirdWatcherImages/Anous minutus_Black Noddy.png")
+    image_path6 <- reactiveVal("BirdNETmonitor/BirdWatcherImages/Anous stolidus_Brown Noddy.png")
+    image_path7 <- reactiveVal("BirdNETmonitor/BirdWatcherImages/Anser albifrons_Greater White-fronted Goose.png")
+    image_path8 <- reactiveVal("BirdNETmonitor/BirdWatcherImages/Anser caerulescens_Snow Goose.png")
+    image_path9 <- reactiveVal("BirdNETmonitor/BirdWatcherImages/Ardea herodias_Great Blue Heron.png")
+    image_path10 <- reactiveVal("BirdNETmonitor/BirdWatcherImages/Ardenna grisea_Sooty Shearwater.png")
+    image_path11 <- reactiveVal("BirdNETmonitor/BirdWatcherImages/Ardenna pacifica_Wedge-tailed Shearwater.png")
 
-    bird_links <- c(
-      "https://ebird.org/species/redava",
-      "https://ebird.org/species/norpin",
-      "https://ebird.org/species/gnwtea",
-      "https://ebird.org/species/mallard",
-      "https://ebird.org/species/blackno",
-      "https://ebird.org/species/brownno",
-      "https://ebird.org/species/whifro",
-      "https://ebird.org/species/snowgo",
-      "https://ebird.org/species/grebhe",
-      "https://ebird.org/species/sooshe",
-      "https://ebird.org/species/wetshe"
-    )
 
-    for (i in 1:11) {
-      output[[paste0("imageDisplay", i)]] <- renderImage({
-        list(src = image_paths[i], height = 300)
-      })
 
-      observeEvent(input[[paste0("imageDisplay", i)]], {
-        shinyjs::enable(ns(paste0("imageDisplay", i)))
-        shinyjs::onclick(
-          selector = paste0("#", ns(paste0("imageDisplay", i))),
-          code = sprintf('window.open("%s", "_blank");', bird_links[i]),
-          delete_file = FALSE
-        )
-      })
-    }
+    observeEvent(input$imageDisplay1, {
+        output$imageOutput1 <- renderImage({
+          list(src = img_path1,
+               style = "max-width:100%",
+               alt = "image not found")
+        }, ignoreInit = TRUE)
+    })
+
+
   })
 }
-
-shinyApp(Wiki, wiki_server)
