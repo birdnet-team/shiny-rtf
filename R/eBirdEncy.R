bae7944d809419db482e2a15ab5a2add58d19530
Melissa Weidlich-Rau <113716944+xxelementsxx@users.noreply.github.com>
2023-11-27 03:07:34 +0100

    fyW mwr xx999

--- R/eBirdEncy.R
+++ R/eBirdEncy.R
@@ -5,28 +5,22 @@ if (!require("shinyjs")) install.packages("shinyjs")
 library(shiny)
 library(shinyjs)
 
-# Funktion zum Laden von Bildern
-img1 <- function(src, alt, style) {
+img1 <- function(src, alt, style = "") {
   tags$img(src = src, alt = alt, style = style)
 }
 
 # Shiny App Definition
-Wiki <- function(id) {
+Wiki <- function(id, image_paths = character(0)) {
   ns <- NS(id)
-
-  image_paths <- c(
-    "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImagesXX/1_Amandava_amandava_Red_Avadavat.png",
-    "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImagesXX/2_Northern_Pintails_(Male_&_Female)_I_IMG_0911.png",
-    # ... (Weiterhin alle Bildpfade hinzufügen)
-  )
 
   fluidPage(
     titlePanel("Hawaiian Birdwatching Encyclopedia -- work in progress mwrxx999"),
     useShinyjs(),
     fluidRow(
       lapply(seq_along(image_paths), function(i) {
+        image_path <- image_paths[i]
         tagList(
-          img1(src = image_paths[i], alt = paste0("image", i), style = "width:300px;height:300px;"),
+          img1(src = image_path, alt = paste0("image", i), style = "width:300px;height:300px;"),
           uiOutput(ns(paste0("image_output", i)))
         )
       })
@@ -35,15 +29,30 @@ Wiki <- function(id) {
 }
 
 # Server-Funktion für die Shiny App
-wiki_server <- function(id, data) {
+wiki_server <- function(id, image_paths = character(0)) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
 
-    for (i in seq_along(data$image_paths)) {
-      download.file(data$image_paths[i], paste0("www/", basename(data$image_paths[i])), mode = "wb")
+    for (i in seq_along(image_paths)) {
+      download.file(image_paths[i], paste0("www/", basename(image_paths[i])), mode = "wb")
       output[[ns(paste0("image_output", i))]] <- renderImage({
-        img1(src = paste0("www/", basename(data$image_paths[i])), style = "max-width:100%", alt = paste0("Bild ", i, " nicht gefunden"))
+        img1(src = paste0("www/", basename(image_paths[i])), style = "max-width:100%", alt = paste0("Bild ", i, " nicht gefunden"))
       }, deleteFile = FALSE)
     }
   })
 }
+
+# Example usage
+image_paths <- c(
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImagesXX/1_Amandava_amandava_Red_Avadavat.png",
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImagesXX/2_Northern_Pintails_(Male_&_Female)_I_IMG_0911.png",
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImagesXX/3_Green-winged_Teal,_Port_Aransas,_Texas.png",
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImages/Anas platyrhynchos_Mallard.png",
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImages/Anous minutus_Black Noddy.png",
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImages/Anous stolidus_Brown Noddy.png",
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImages/Anser albifrons_Greater White-fronted Goose.png",
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImages/Anser caerulescens_Snow Goose.png",
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImages/Ardea herodias_Great Blue Heron.png",
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImages/Ardenna grisea_Sooty Shearwater.png",
+  "C:/Users/ElementXX/Desktop/RSTudioNshinYXX888/FrontEnd999XX/MRWFrontE999XX/BirdNETmonitor/BirdWatcherImages/Ardenna pacifica_Wedge-tailed Shearwater.png"
+)
